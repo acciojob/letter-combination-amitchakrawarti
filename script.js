d87eed5563ab074138d6e82379c0244178084b63
amitchakrawarti <119115364+amitchakrawarti@users.noreply.github.com>
2023-02-17 15:16:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
-function letterCombinations(input_digit) {
-  //Complete the function
-}
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser')
 
-module.exports = letterCombinations;
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+const letterCombination = require('./script');
+
+app.use(express.static(__dirname))
+
+app.post('/lettercomb',(req, res) => {
+  const input = req.body.num
+  const answer = letterCombination(input) 
+  res.send({message:JSON.stringify(answer)})
+})
+
+module.exports = app;
